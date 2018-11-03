674ca0f01f06c583c3b481fdb67c7cfd53a4becb
Blazer <sstubbs012@gmail.com>
2018-11-03 16:56:44 -0400

    some working code

---
+++ config/orm.js
@@ -0,0 +1,21 @@
+var connection = require("./connection.js");
+
+var orm = {
+    selectAll: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: function() {
+
+    },
+    updateOne: function() {
+
+    }
+};
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", true);
