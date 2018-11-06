807b58efdfa661109b49fcb36d6f8660de9e6729
Blazer <sstubbs012@gmail.com>
2018-11-05 21:57:00 -0500

    almost there. Need to update routes

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,38 @@
+var express = require("express");
+
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burger_name"
+    ], [
+        req.body.burger_name
+    ], function(result) {
+        res.redirect("/");
+    });
+});
+
+router.put("/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: true
+    }, condition, function(result) {
+        res.redirect("/");
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var router = require("./controllers/burgers_controller.js");
+var path = require("path");
+var methodOverride = require("method-override");
 
 var PORT = process.env.PORT || 8080;
 
 var app = express();
+
+// Middleware
+app.use(methodOverride("_method"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Set up rendering engine, Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Static files and routes (middleware)
+app.use(express.static(__dirname + "/public"));
+app.use("/", router);
+
+// Start server
+app.listen(PORT, function() {
+	console.log("Listening on PORT " + PORT);
+});
